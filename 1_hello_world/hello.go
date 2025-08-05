5006dccde2a4e64e449f621088d86ac4f6299984
Av1nay <avi9071@gmail.com>
2025-08-05 22:18:37 +0300

    hellow world folder created

---
+++ 1_hello_world/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")// This prints "Hello, World!" followed by a newline character.
+	fmt.Print("this is a simple Go program that prints a message to the console without line breaks.")// This prints a message to the console without adding a newline at the end.
+	fmt.Printf(" Printf formats according to a format specifier and writes to standard output.") // This prints a formatted string to the console, useful for more complex output formatting.
+
+}
