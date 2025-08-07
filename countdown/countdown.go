8f0dd17c82425c993fd7df742215fa6046446854
Av1nay <avi9071@gmail.com>
2025-08-07 14:34:56 +0300

    count down fixed

---
+++ countdown/countdown.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+func Countdown(n int) string{
+	result:=""
+	if n==0{
+		return "0!"
+	}
+	for i:=n; i>0; i-=2{
+		result+=string('0'+i)+","
+		
+	}
+	result+="0!"
+	return result
+}
+
+func main(){
+	fmt.Println(Countdown(8))
+}
