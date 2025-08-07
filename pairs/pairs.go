b6b7ec47b3e8d8908f4a7b9eb18eb64afaa704db
Av1nay <avi9071@gmail.com>
2025-08-07 12:24:52 +0300

    pair added

---
+++ pairs/pairs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func Pairs() string {
+	result :="" // initialize the empty string to append the output
+
+	for i:=0; i<=98; i++{ //looping through the first number 0 to 98
+		for j:=i+1; j<=99; j++{ //looping through the second number from 1 to 99
+			pair:=fmt.Sprintf("%02d %02d", i, j) // padding 0 with 2 digit width of the decimal d
+
+			if result !=""{ //adding the ", " before upcomming pair
+				result+=", "
+			}
+			result +=pair
+		}
+	}
+	return result
+}
+
+func main(){
+	fmt.Println(Pairs())
+}
