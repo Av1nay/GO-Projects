592f0627b2dd1e9be9c959bd2d651b3eca9b9723
Av1nay <avi9071@gmail.com>
2025-08-08 11:14:50 +0300

    countdown modified

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-
-func Countdown(n int) string{
-	result:=""
-	if n==0{
+func Countdown(n int) string {
+	result := ""
+	if n == 0 {
 		return "0!"
 	}
-	for i:=n; i>0; i-=2{
-		result+=string('0'+i)+","
-		
+	for i := n; i > 0; i -= 2 {
+		result += string('0'+i) + ","
+
 	}
-	result+="0!"
+	result += "0!"
 	return result
 }
 
-func main(){
-	fmt.Println(Countdown(8))
+func main() {
+	fmt.Println(Countdown(7))
 }
