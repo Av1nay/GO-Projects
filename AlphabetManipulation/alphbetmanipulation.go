ecb1d8d43019d85a5b736c5cc87c1dd6dc16ab9e
Av1nay <avi9071@gmail.com>
2025-08-08 11:15:20 +0300

    alphabetmanipulation added

--- AlphabetManipulation/alphbetmanipulation.go
+++ AlphabetManipulation/alphbetmanipulation.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func num(n int) string{
-	if n > 0 && n < 27{ // ensuring the n is inside the range
-		result := ""  // initializing a empty result where each letter will be appended
-		for i:=0; i<n; i++{  //loops from 0 to n-1
-			alphabet := rune('a' + i)  //rune ensure working with Unicode characters and adding i gives next letters if i is 0 then alphabet will be a, if 1 then b
+func num(n int) string {
+	if n > 0 && n < 27 { // ensuring the n is inside the range
+		result := ""             // initializing a empty result where each letter will be appended
+		for i := 0; i < n; i++ { //loops from 0 to n-1
+			alphabet := 'a' + i        //rune ensure working with Unicode characters and adding i gives next letters if i is 0 then alphabet will be a, if 1 then b
 			result += string(alphabet) // converts rune to string and append the output to result
 		}
 		return result //returning the result here
@@ -14,7 +14,6 @@ func num(n int) string{
 	return "Please input from 1 to 26" // returning error or invalid input
 }
 
-
-func main(){
-	fmt.Println(num(12))
+func main() {
+	fmt.Println(num(1))
 }
